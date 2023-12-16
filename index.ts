e39020c0fc532c13683d506c8d38f4d98ee9f078
Nafeu Nasir <nafeu@tenjin.com>
2023-12-16 18:14:35 -0500

    Add parseConceptMap function and specs

--- index.ts
+++ index.ts
@@ -1,14 +1,69 @@
-export function helloWorld() {
-  const message = 'Hello World from my example modern npm package!';
-  return message;
-}
+const DEFAULT_DELIMITER = '\n';
+const DEFAULT_ID_SYMBOL = '#';
+const FIRST_INDEX = 0;
+const SECOND_INDEX = 1;
+const MIN_CONCEPT_KEY_LENGTH = 1;
+const MAX_DELIMITER_LENGTH = 1;
 
-export function goodBye() {
-  const message = 'Goodbye from my example modern npm package!';
-  return message;
-}
-
-export default {
-  helloWorld,
-  goodBye,
+type ParsingOptions = {
+  delimiter?: string;
+  idSymbol?: string;
 };
+
+type Concepts = {
+  [key: string]: string[];
+};
+
+type ConceptMap = {
+  concepts: Concepts;
+  root: string | null;
+};
+
+export function parseConceptMap(text: string, options: ParsingOptions = {}): ConceptMap {
+  const isMissingText: boolean = text === null || text === undefined || text.length === 0;
+
+  if (isMissingText) {
+    throw new Error('Missing concept map text parameter');
+  }
+
+  const { delimiter = DEFAULT_DELIMITER, idSymbol = DEFAULT_ID_SYMBOL } = options;
+
+  if (idSymbol.length > MAX_DELIMITER_LENGTH) {
+    throw new Error(`ID symbol "${idSymbol}" too long, must be one character`);
+  }
+
+  const concepts: Concepts = {};
+
+  const conceptText = text.split(delimiter).filter((item) => {
+    const isItemNotEmpty = item.length > 0 && item !== '';
+
+    return isItemNotEmpty;
+  });
+
+  let activeKey: string | null = null;
+  let root: string | null = null;
+
+  for (const line of conceptText) {
+    const isConceptKey = line[FIRST_INDEX] === idSymbol && line.length > MIN_CONCEPT_KEY_LENGTH;
+
+    if (isConceptKey) {
+      activeKey = line.substring(SECOND_INDEX);
+
+      if (root === null) {
+        root = activeKey;
+      } else if (concepts.hasOwnProperty(activeKey)) {
+        throw new Error(`Duplicate id: ${idSymbol}${activeKey}`);
+      }
+    } else if (activeKey) {
+      concepts[activeKey] = concepts[activeKey] ? [...concepts[activeKey], line] : [line];
+    }
+  }
+
+  if (root === null) {
+    throw new Error('Missing root id');
+  }
+
+  return { concepts, root };
+}
+
+export default { parseConceptMap };
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,19 +1,120 @@
-import { describe, expect, test } from '@jest/globals';
-import { helloWorld } from '../index';
+import { describe, expect, it } from '@jest/globals';
+import { parseConceptMap } from '../index';
 
-describe('helloWorld', () => {
-  test('returns a string', () => {
-    const result = helloWorld();
-    expect(typeof result).toBe('string');
+describe('parseConceptMap', () => {
+  describe('given a null or empty text value', () => {
+    it('should throw an error', () => {
+      expect(() => parseConceptMap('')).toThrow('Missing concept map text parameter');
+      expect(() => parseConceptMap(null)).toThrow('Missing concept map text parameter');
+    });
   });
+  describe('given a non-empty string', () => {
+    describe('with no id symbol', () => {
+      it('should throw an error', () => {
+        const exampleInput = 'root\nexample';
 
-  test('returns a greeting message', () => {
-    const result = helloWorld();
-    expect(result).toMatch(/Hello World/);
+        expect(() => parseConceptMap(exampleInput)).toThrow('Missing root id');
+      });
+    });
+    describe('with a flat heirarchy', () => {
+      it('should return a valid concept map with root string and no issues', () => {
+        const exampleInput = '#root\nexample';
+
+        const result = parseConceptMap(exampleInput);
+
+        expect(result).toEqual({
+          concepts: {
+            root: ['example'],
+          },
+          root: 'root',
+        });
+      });
+      describe('with duplicate lines', () => {
+        it('should return a valid concept map with root string and no issues', () => {
+          const exampleInput = '#root\nexample\nexample';
+
+          const result = parseConceptMap(exampleInput);
+
+          expect(result).toEqual({
+            concepts: {
+              root: ['example', 'example'],
+            },
+            root: 'root',
+          });
+        });
+      });
+    });
+    describe('with a deep heirarchy', () => {
+      it('should return a valid concept map with root string and no issues', () => {
+        const exampleInput = '#x\n[a]\n#a\n[b]\n#b\nc';
+
+        const result = parseConceptMap(exampleInput);
+
+        expect(result).toEqual({
+          concepts: {
+            x: ['[a]'],
+            a: ['[b]'],
+            b: ['c'],
+          },
+          root: 'x',
+        });
+      });
+      describe('with a duplicate concept id', () => {
+        it('should throw an error', () => {
+          const exampleInput = '#x\n[a]\n#a\n[b]\n#a\n[d]\n#b\nc';
+
+          expect(() => parseConceptMap(exampleInput)).toThrow('Duplicate id: #a');
+        });
+      });
+    });
   });
+  describe('given custom options', () => {
+    describe('with a deep heirarchy', () => {
+      describe('with a custom delimiter', () => {
+        it('should return a valid concept map with root string and no issues', () => {
+          const exampleInput = '#x;[a];#a;[b];#b;c';
+          const options = { delimiter: ';' };
 
-  test('includes package-specific message', () => {
-    const result = helloWorld();
-    expect(result).toContain('example modern npm package');
+          const result = parseConceptMap(exampleInput, options);
+
+          expect(result).toEqual({
+            concepts: {
+              x: ['[a]'],
+              a: ['[b]'],
+              b: ['c'],
+            },
+            root: 'x',
+          });
+        });
+      });
+      describe('with a custom idSymbol', () => {
+        it('should return a valid concept map with root string and no issues', () => {
+          const exampleInput = '|x\n[a]\n|a\n[b]\n|b\nc';
+          const options = { idSymbol: '|' };
+
+          const result = parseConceptMap(exampleInput, options);
+
+          expect(result).toEqual({
+            concepts: {
+              x: ['[a]'],
+              a: ['[b]'],
+              b: ['c'],
+            },
+            root: 'x',
+          });
+        });
+
+        describe('with an idSymbol longer than one character', () => {
+          it('should throw an error', () => {
+            const exampleInput = '--x\n[a]\n--a\n[b]\n--b\nc';
+            const options = { idSymbol: '--' };
+
+            expect(() => parseConceptMap(exampleInput, options)).toThrow(
+              'ID symbol "--" too long, must be one character',
+            );
+          });
+        });
+      });
+    });
   });
 });
